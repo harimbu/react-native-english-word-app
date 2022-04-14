cdc341e97b9bf06e76a4c5064635a3ecae2b91f5
harimbu <harimbu@gmail.com>
2022-04-15 03:19:55 +0900

    sqlite

--- screens/Home.js
+++ screens/Home.js
@@ -1,3 +1,6 @@
+import * as FileSystem from 'expo-file-system'
+import { Asset } from 'expo-asset'
+import * as SQLite from 'expo-sqlite'
 import { View, Text, FlatList, TouchableOpacity } from 'react-native'
 import { useState, useEffect } from 'react'
 import tw from 'twrnc'
@@ -7,16 +10,50 @@ import { useRecoilValue } from 'recoil'
 import { rangeState } from '../store'
 import Ionicons from '@expo/vector-icons/Ionicons'
 
-export default function Hime() {
+export default function Home() {
   const [words, setWords] = useState([])
+  const [english, setEnglish] = useState([])
   const range = useRecoilValue(rangeState)
 
+  // useEffect(() => {
+  //   setWords(db.words.filter(item => item.range === range))
+  // }, [range])
+
   useEffect(() => {
-    setWords(db.words.filter(item => item.range === range))
+    openDatabase()
+  }, [])
+
+  useEffect(() => {
+    if (words) {
+      setEnglish(words.filter(item => item.range === `${range}`))
+    }
   }, [range])
 
+  async function openDatabase() {
+    if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
+      await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite')
+    }
+    await FileSystem.downloadAsync(
+      Asset.fromModule(require('../assets/words.db')).uri,
+      FileSystem.documentDirectory + 'SQLite/words.db'
+    )
+    const db = SQLite.openDatabase('words.db')
+
+    db.transaction(tx => {
+      tx.executeSql('SELECT * FROM words', [], (tx, results) => {
+        let items = []
+        for (let i = 0; i < results.rows.length; i++) {
+          items.push(results.rows.item(i))
+        }
+        setWords(items)
+      })
+    })
+  }
+
+  console.log(english.length)
+
   const Item = ({ eng, kor }) => (
-    <View style={tw`flex flex-row p-2`}>
+    <View style={tw`flex flex-row px-2 py-2`}>
       <View style={tw`w-2/4`}>
         <Text>{eng}</Text>
       </View>
@@ -26,9 +63,9 @@ export default function Hime() {
           <TouchableOpacity>
             <Ionicons name="eye" size={22} />
           </TouchableOpacity>
-          {/* <TouchableOpacity style={tw`ml-2`}>
+          <TouchableOpacity style={tw`ml-2`}>
             <Ionicons name="add" size={22} />
-          </TouchableOpacity> */}
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -41,7 +78,7 @@ export default function Hime() {
       <Range />
       <View style={tw`p-3`}>
         <FlatList
-          data={words}
+          data={english}
           renderItem={renderItem}
           keyExtractor={item => item.no}
           showsVerticalScrollIndicator={false}
